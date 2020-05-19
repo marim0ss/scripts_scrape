cc6aca278cea7d853307055f009481336f247ced
marim0ss <piyomaru.bunbun@gmail.com>
2020-05-19 20:34:59 +0900

    キーワード入力→検索ボタンを押す

--- syohyo_browser_scrape.py
+++ syohyo_browser_scrape.py
@@ -12,16 +12,23 @@ import pprint  # pprint.pprint()で出力が改行されて見やすくなる
 def print_html(x):
     print(bs4.BeautifulSoup(x.get_attribute('innerHTML'), 'html.parser').prettify())
 
+seach_word = "Ｃａｒｅｅｒ Ｂｏｏｓｔ"
 # ブラウザ起動
 browser = webdriver.Chrome()
 
 # サイトに移動
 URL = 'https://www.j-platpat.inpit.go.jp/s0100'
 browser.get(URL)
-# ラジオボタン商標を選択
+# 商標を選択
 radiobtn = browser.find_element_by_xpath("//*[@id='mat-radio-5']/label/div[1]")
 radiobtn.click()
 
+form_area = browser.find_element_by_tag_name('form') # 簡易検索エリア
+form_text = form_area.find_element_by_name("s01_srchCondtn_txtSimpleSearch")
+# form_text.clear() # 繰り返し検索時、すでに入っているキーワードは消す
+form_text.send_keys(seach_word)
+search_btn = form_area.find_element_by_name('s01_srchBtn_btnSearch')
+search_btn.click()
 time.sleep(3)
 
 browser.close()
