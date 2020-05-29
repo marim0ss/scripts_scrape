062113a95f3d2211c783f3d4db61aa1683539269
marim0ss <piyomaru.bunbun@gmail.com>
2020-05-29 22:45:59 +0900

    delete files

--- driver_open_basic.py
+++
@@ -1,9 +0,0 @@
-#coding: UTF-8
-import time                            # スリープを使うために必要
-from selenium import webdriver         # Webブラウザを自動操作する（python -m pip install selenium)
-import chromedriver_binary             # パスを通すためのコード
-
-driver = webdriver.Chrome()            # Chromeを準備
-driver.get('https://www.google.com/')  # Googleを開く
-time.sleep(10)                          # 10秒間待機
-driver.quit()                          # ブラウザを閉じる
